514676910776cfb36aaa154b7a95d7ee3cfec808
Kyrre Havik Eriksen <Kyrremann@gmail.com>
2016-10-06 23:24:10 +0200

    Simple display of how the layout can be

--- js/app.js
+++ js/app.js
@@ -62,31 +62,83 @@ var data = [
 	    "pulje_slutt": 9,
 	    "kategori": "brettspill"
 	}
-    ]
+]
 
+puljer = {
+    "1": "19-21",
+    "2": "22-24",
+    "3": "01-03",
+    "4": "10-12",
+    "5": "13-15",
+    "6": "16-18",
+    "7": "19-21",
+    "8": "22-24",
+    "9": "01-03",
+    "10": "10-12",
+    "11": "13-15",
+    "12": "16-18",
+    "13": "19-21",
+    "14": "22-24",
+    "15": "01-03",
+    "16": "11-13",
+    "17": "14-16"
+}
+
+function getTimeForPule(pulje) {
+    return puljer[pulje.toString()];
+}
+
+// List.js features
 var options = {
-    valueNames: [ 'navn', 'pulje_start' ],
-    item: '<li class="pulje"><h3 class="navn"></h3><p class="pulje_start"></p></li>'
+    valueNames: [ 'navn', 'pulje_start', 'beskrivelse' ],
+    item: '<li class="pulje">\
+<strong class="navn"></strong>\
+<br />\
+<img src="http://www.spillfestival.no/images/happy.gif" alt="Ingen regelkunnskaper kreves">\
+<br />\
+<strong>Type:</strong> Brettspill\
+<br />\
+<strong>Arrangør:</strong> Kyrre Havik Eriksen\
+<span class="label label-info pull-right pulje_start"></span>\
+<br />\
+<br />\
+<button class="button-xsmall pure-button-primary pure-button toggleDescription">Mer info</button>\
+<div class="slideDescription">\
+<strong>Maks deltagere:</strong> 8\
+<br />\
+<strong>Vinnertype:</strong> Vinner kåres\
+<br />\
+<strong>Beskrivelse:</strong>\
+<div class="beskrivelse"></div>\
+</div>\
+</li>'
 };
 
 var programList = new List('program', options, data);
 programList.sort('pulje_start', { order: "asc" });
 
-$('.filter-clear').on('click', function() {
-    programList.filter();
-    programList.sort('pulje_start', { order: "asc" });
-});
-
-$('.filter-kortspill').on('click', function() {
-    programList.filter(function(item) {
-	var kategori = item.values().kategori;
-	return kategori === "kortspill";
+$(document).ready(function(e) {
+    $('.filter-clear').on('click', function() {
+	programList.filter();
+	programList.sort('pulje_start', { order: "asc" });
     });
-});
 
-$('.filter-brettspill').on('click', function() {
-    programList.filter(function(item) {
-	var kategori = item.values().kategori;
-	return kategori === "brettspill";
+    $('.filter-kortspill').on('click', function() {
+	programList.filter(function(item) {
+	    var kategori = item.values().kategori;
+	    return kategori === "kortspill";
+	});
+    });
+
+    $('.filter-brettspill').on('click', function() {
+	programList.filter(function(item) {
+	    var kategori = item.values().kategori;
+	    return kategori === "brettspill";
+	});
+    });
+
+    // JQuery features
+    $(".toggleDescription").click(function() {
+	$(this).next().first().slideToggle();
     });
 });
